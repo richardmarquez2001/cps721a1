9eecc523170de20251591f26d28e0b95a68cb6be
Ethan Hou <ethanykhou@gmail.com>
2022-09-25 21:39:56 -0400

    Q1 complete
    q1 completed both KB and queries

--- question1_books.pl
+++ question1_books.pl
@@ -13,15 +13,40 @@
 
 %%%%% ATOMIC: authorOf
 % Add the atomic propositions for authorOf in this section
-authorOf(ben, ethan).
+authorOf(chimamanda_ngozi_adichie, half_of_a_yellow_sun).
+authorOf(mordecai_richler, the_apprenticeship_of_duddy_kravitz).
+authorOf(mordecai_richler, barneys_version).
+authorOf(stephen_king, it), 
+authorOf(stephen_king, the_stand)
+authorOf(haruki_murakami, a_wild_sheep_chase)
+authorOf(haruki_murakami, hear_the_wind_sing)
+authorOf(nora_roberts, angels_fall)
 
 %%%%%  ATOMIC: publishedBy
 % Add the atomic propositions for publishedBy in this section
+
+publishedBy(pan_macmillan, station_eleven)
+publishedBy(simon_&_schuster, it)
+publishedBy(doubleday, the_stand)
+publishedBy(reganbooks, wicked)
+publishedBy(reganbooks, the_truth)
+publishedBy(macmillan_publishers, charles_kingsleys_westward_ho!) 
+publishedBy(macmillan_publishers, the_jungle_book)
 
 
 %%%%% ATOMIC: publicationYear
 % Add the atomic propositions for publicationYear in this section
 
+publicationYear(the_testaments, 2019)
+publicationYear(a_wild_sheep_chase, 1982)
+publicationYear(hear_the_wind_sing, 1979)
+publicationYear(angels_fall, 2006)
+publicationYear(high_noon, 2007)
+publicationYear(the_hollow, 2008)
+publicationYear(wicked, 1995)
+publicationYear(the_truth, 2015)
+publicationYear(charles_kingsleys_westward_ho!, 1855)
+publicationYear(the_jungle_book, 1894)
 
 %%%%% END
 % DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
